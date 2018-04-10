bf405ba036c6c7a1db2d48cecefb69b731c4cc32
DEVELOPMENT\leonardo.moreira <leonardo.moreira@fleetmatics.com>
2018-04-10 15:25:06 +0100

    testing add hello page

---
+++ src/pages/hello/hello.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-hello',
+    templateUrl: 'hello.html'
+})
+
+export class HelloPage {
+    constructor(public navCtrl: NavController){
+        
+    }
+}
